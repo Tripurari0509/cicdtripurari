bd87ea14f1228c612e3035f77ea1b0d9d80a139d
deepanshu1994 <47136571+deepanshu1994@users.noreply.github.com>
2019-04-15 11:10:28 +0530

    Update HelloWorld.java
    duplicate values

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -9,5 +9,11 @@ public class HelloWorld {
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
+	System.out.println(greeter.sayHello());
+	System.out.println(greeter.sayHello());
+	System.out.println(greeter.sayHello());
+	System.out.println(greeter.sayHello());   
+	System.out.println(greeter.sayHello());
+	 
     }
 }
